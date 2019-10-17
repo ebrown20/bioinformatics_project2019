958c0fec7169d0b187c74a4d3bca6fdc80fc6cc6
Alexis Driscoll <adrisco2@crcfe01.crc.nd.edu>
2019-10-16 22:48:02 -0400

    commented projectscript.sh

--- projectscript.sh
+++ projectscript.sh
@@ -21,15 +21,20 @@ done
 ./../Private/bin/muscle3.8.31_i86linux64 -in hsp70geneALL.fasta -out hsp70geneAligned.fasta
 ./../Private/bin/hmmbuild hsp70geneSequence.fasta hsp70geneAligned.fasta
 
+#loops through all proteomes in the proteomes folder
 for file in proteomes/proteome*.fasta
 do
+ #extracts the proteome part of the file name
  filename=$(echo $file | cut -d / -f 2)
+ #searches for an mcrAgene in each proteome file
  ./../Private/bin/hmmsearch -o mcrAgeneFound_$filename mcrAgeneSequence.fasta $file
  hits=$(grep 'No hits' mcrAgeneFound_$filename | wc -l)
+ #if there are no hits, file is removed
  if [ $hits -gt 0 ]
  then
   rm mcrAgeneFound_$filename
  fi
+ #if there is at least one hit, proteome name/number outputted to file and looks for hsp70genes 
  if [ $hits -eq 0 ]
  then
   echo $filename >> mcrAgeneFound.txt
@@ -38,11 +43,13 @@ do
   do
    ./../Private/bin/hmmsearch -o hsp70geneFound_$line hsp70geneSequence.fasta proteomes/$line
   hsphits=$(grep 'No hits' hsp70geneFound_$line | wc -l)
+  #removes file if no hsp70gene hits
   if [ $hsphits -gt 0 ]
   then
    rm hsp70geneFound_$line
   fi
   done
+  #if there were hsp70genes, outputs proteome name/number to file and number of hsp70genes found in that proteome
   if [ $hsphits -eq 0 ]
   then
    echo $line >> hsp70geneFound.txt
